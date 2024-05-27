9b7c3d88c5d0b1b52ee349e31b87b4f7a36208c5
Prem9910-p <prem60866@gmail.com>
2024-05-27 12:58:18 +0530

    Initial commit one

---
+++ src/main/java/com/example/VacciNation/Enum/DayOfWeek.java
@@ -0,0 +1,11 @@
+package com.example.VacciNation.Enum;
+
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/main/java/com/example/VacciNation/VaccineNationApplication.java
@@ -0,0 +1,15 @@
+package com.example.VacciNation;
+
+import com.example.VacciNation.Enum.DayOfWeek;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineNationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(VaccineNationApplication.class, args);
+
+	}
+}
